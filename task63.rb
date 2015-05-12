08d21ea3e0229daebf1d2bf2ca7f3c264d130339
Andrey Demidenko <demidenkoandy@gmail.com>
2015-05-12 15:29:42 +0300

    Add task#63

--- task63.rb
+++ task63.rb
@@ -1,5 +1,20 @@
 # №63 Дан целочисленный массив. Найти максимальное количество
 # подряд идущих минимальных элементов.
 
-def task63(arary)
+def task63(array)
+  min = array.min
+  return 1 if array.count(min) == 1
+  count = 1;
+  maxcount = 1;
+  for i in 0...array.size
+    if array[i] == min && array[i] == array[i+1]
+      count += 1
+      maxcount = count if maxcount < count
+    else
+      count = 1
+    end
+  end
+  maxcount
 end
+
+p task63([3,1,1,1,1,2,1,1,4,1,1,1,2])
