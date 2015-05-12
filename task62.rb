9b2351b195af467991fa6c3403b8a4476672acfe
Andrey Demidenko <demidenkoandy@gmail.com>
2015-05-12 15:05:50 +0300

    Add task#62

---
+++ task62.rb
@@ -0,0 +1,10 @@
+# №62 Дан целочисленный массив. Найти два наименьших элемента.
+
+def task62(array)
+  array2 = array.dup
+  [array2.delete(array2.min), array2.delete(array2.min)]
+end
+
+a = [1,2,3,4,23,5,1,17]
+p task62(a)
+p a
