7d23b1feb0fb6f43dc6fe4a08db95677d3835d15
Andrey Demidenko <demidenkoandy@gmail.com>
2015-05-12 16:33:59 +0300

    Add task#70

---
+++ task70.rb
@@ -0,0 +1,20 @@
+# №70 Дано вещественное число R и массив вещественных чисел.
+# Найти два элемента массива, сумма которых наименее близка к данному числу.
+
+def task70(array, number)
+  max = (array[0] + array[1] - number).abs
+  el1 = array[0]
+  el2 = array[1]
+  result = [array[0], array[1]]
+  for i in 0...array.size
+    for j  in (i + 1)...array.size
+      if (array[i] + array[j] - number).abs > max
+      max = (array[i] + array[j] - number).abs
+      result = [array[i], array[j]]
+      end
+    end
+  end
+  result
+end
+
+p task70([1.2,1.3,1.35,1.40,1.1], 1.33)
