c1a4b1f2fb2620f666a351dd7f76eadda6e49512
Andrey Demidenko <demidenkoandy@gmail.com>
2015-05-12 14:56:15 +0300

    Add task#59

---
+++ task59.rb
@@ -0,0 +1,8 @@
+# №59 Дан целочисленный массив. Найти количество элементов,
+# между первым и последним минимальным.
+
+def task59(array)
+  array[(array.index(array.min) + 1)...array.rindex(array.min)].count
+end
+
+p task59([5,3,1,5,6,2,1,5])
