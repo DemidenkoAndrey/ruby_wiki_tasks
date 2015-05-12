f7fb20a0431dfb6ef46c843c41dc66dd135117a7
Andrey Demidenko <demidenkoandy@gmail.com>
2015-05-12 12:05:19 +0300

    Add task#23

---
+++ task22.rb
@@ -0,0 +1,24 @@
+# №22 Дан целочисленный массив. Определить количество
+# участков, на которых его элементы монотонно убывают.
+
+def task22(array)
+  result = 0
+  for i in 2..array.size
+    array.each_cons(i) do |a|
+      p a
+      flag = false
+      for j in 0...a.size-1
+        if a[j] > a[j+1]
+          flag = true
+        else
+          flag = false
+          break
+        end
+      end
+      result += 1 if flag
+    end
+  end
+  result
+end
+
+puts task22([1,4,3,4])
---
+++ task23.rb
@@ -0,0 +1,17 @@
+# №23 Дано вещественное число R и массив вещественных чисел.
+# Найти элемент массива, который наиболее близок к данному числу.
+
+def task23(array, number)
+  min = (array[0] - number).abs
+  el = array[0]
+  array.each do |v|
+    if (number - v).abs < min
+      min = (number - v).abs
+      el = v
+    end
+  end
+  el
+end
+
+
+p task23([1.2,1.3,1.35,1.40], 1.33)
