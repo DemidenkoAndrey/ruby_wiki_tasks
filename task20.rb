634f612f842bde0af4d08c885968860230208a0a
Andrey Demidenko <demidenkoandy@gmail.com>
2015-05-12 10:54:34 +0300

    Add task#20

---
+++ task20.rb
@@ -0,0 +1,14 @@
+# №20 Дан целочисленный массив. Найти минимальный
+# из его локальных минимумов.
+
+def task19(array)
+  array_result = []
+  array_result << array[0] if array[0] < array[1]
+  array_result << array[-1] if array[-1] < array[-2]
+  for i in 1...array.size-1
+    array_result << array[i] if array[i] < array[i+1] && array[i] < array[i-1]
+  end
+  array_result.min
+end
+
+puts task19([1,2,15,3,5,15,12,15])
