7bf67d4494bcd05e85b51ab31d5d283fae2aad97
Andrey Demidenko <demidenkoandy@gmail.com>
2015-05-12 16:11:50 +0300

    Add task#68

---
+++ task68.rb
@@ -0,0 +1,10 @@
+# Дан целочисленный массив. Проверить, чередуются ли
+# в нем положительные и отрицательные числа.
+
+def task68(array)
+  res = array.partition.with_index { |_, index| index.even? }
+  (res[0].all? { |v| v > 0 } && res[1].all? { |v| v < 0 }) ||
+  (res[0].all? { |v| v < 0 } && res[1].all? { |v| v > 0 })
+end
+
+p task68([2,-1,2,0,3,-10])
