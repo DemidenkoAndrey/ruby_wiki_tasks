da25b52551ac2f425c2905b78be2a3c4135dc16b
Andrey Demidenko <demidenkoandy@gmail.com>
2015-05-12 15:15:54 +0300

    Fix task#1 and task#2

--- task2.rb
+++ task2.rb
@@ -2,7 +2,7 @@
 # его элементы с нечетными индексами, а затем - четными.
 
 def partition_odd_even(array)
-  array.partition { |v| v.odd?}
+  array.partition.with_index { |_, index| index.odd?}
 end
 
 puts partition_odd_even([1,2,3,4,5,6])
