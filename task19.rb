e6a8d1bec398724ee8a1c0af5a3685e07358752b
Andrey Demidenko <demidenkoandy@gmail.com>
2015-05-12 10:52:46 +0300

    Add task#19

--- task19.rb
+++ task19.rb
@@ -1,1 +1,14 @@
-task19.rb
+# №19 Дан целочисленный массив. Найти максимальный
+# из его локальных максимумов.
+
+def task19(array)
+  array_result = []
+  array_result << array[0] if array[0] > array[1]
+  array_result << array[-1] if array[-1] > array[-2]
+  for i in 1...array.size-1
+    array_result << array[i] if array[i] > array[i+1] && array[i] > array[i-1]
+  end
+  array_result.max
+end
+
+puts task19([1,2,15,3,5,15,12,15])
