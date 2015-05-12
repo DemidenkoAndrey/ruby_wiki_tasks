fa2b6f72a9cfb7af8a3e19ee846736d208f1f3ba
Andrey Demidenko <demidenkoandy@gmail.com>
2015-05-12 13:37:34 +0300

    Add task#27

---
+++ task27.rb
@@ -0,0 +1,18 @@
+# №27 Дан целочисленный массив. Преобразовать его, вставив
+# после каждого положительного элемента нулевой элемент.
+
+def task27(array)
+  i = 0
+  while i < array.size
+    if array[i] > 0
+      array.insert(i+1,array[0])
+      i += 2
+      p i, array.size
+    else
+      i += 1
+    end
+  end
+  array
+end
+
+p task27([1,-5,3,4])
