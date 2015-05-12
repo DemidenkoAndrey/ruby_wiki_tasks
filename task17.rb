add0d712b5ba3e9e7e52dabfd72367ffbaf2679f
Andrey Demidenko <demidenkoandy@gmail.com>
2015-05-12 10:49:34 +0300

    Fix task#17 and task#18

--- task17.rb
+++ task17.rb
@@ -2,7 +2,12 @@
 # его локальных максимумов.
 
 def task17(array)
-  array.count(array.max)
+  count = 0;
+  count += 1 if array[0] > array[1] || array[-1] > array[-2]
+  for i in 1...array.size-1
+    count +=1 if array[i] > array[i+1] && array[i] > array[i-1]
+  end
+  count
 end
 
 puts task17([1,2,15,3,5,15,12,15])
