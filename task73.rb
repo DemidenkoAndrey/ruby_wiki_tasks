2a4d2c6926c8ebcf9ba611288dc0c78ce3ef0713
Andrey Demidenko <demidenkoandy@gmail.com>
2015-05-12 16:48:28 +0300

    Add task#73

---
+++ task73.rb
@@ -0,0 +1,11 @@
+# №73 Дан целочисленный массив. Удалить все элементы,
+# встречающиеся ровно два раза
+
+def task73(array)
+  value_count = Hash.new(0)
+  array.each { |v| value_count[v] +=1 }
+  delete_values = value_count.select { |_, v| v == 2 }.keys
+  array -= delete_values
+end
+
+p task73([1,2,3,4,2,3,6,1,5,3,51])
