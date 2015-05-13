004e9d0289ee79e68507df4a5f984c11e51d9aec
Andrey Demidenko <demidenkoandy@gmail.com>
2015-05-13 11:03:03 +0300

    Add task#101

---
+++ task101.rb
@@ -0,0 +1,14 @@
+# №100 Дан целочисленный массив. Вывести индексы элементов,
+# которые больше своего левого соседа, и количество таких чисел.
+
+def task100(array)
+  result = []
+  for i in 1...array.size
+    if array[i] > array[i-1]
+      result << i
+    end
+  end
+  [result, result.size]
+end
+
+p task100([1,2,3,4,5,3,1])
