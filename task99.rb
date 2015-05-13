eef86b33ba5b7e4b2628c53ab6a0b28f4a9f8198
Andrey Demidenko <demidenkoandy@gmail.com>
2015-05-13 11:00:19 +0300

    Add task#99

---
+++ task99.rb
@@ -0,0 +1,15 @@
+# №99 Дан целочисленный массив. Вывести индексы элементов,
+# которые меньше своего правого соседа, и количество таких чисел.
+
+def task99(array)
+  result = []
+  for i in 0...array.size-1
+    if array[i] < array[i+1]
+      result << i
+    end
+  end
+  [result, result.size]
+end
+
+p task99([1,2,3,4,5,3,1])
+
